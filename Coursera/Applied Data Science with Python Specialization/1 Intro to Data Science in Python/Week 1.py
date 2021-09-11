bc2b0af51ba05d9ea07421810e11a77b3491ae42
Zhou Nan <nanzn.zhou@gmail.com>
2021-09-11 10:51:10 +0800

    Week 1 Part 1

--- Coursera/Applied Data Science with Python Specialization/1 Intro to Data Science in Python/Week 1.py
+++ Coursera/Applied Data Science with Python Specialization/1 Intro to Data Science in Python/Week 1.py
@@ -1,1 +1,7 @@
-print("Test Push")
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+# Generate a list of userid in format 'aa00' consisting of 2 lowercase and 2 digits
+answer = [l1+l2+n1+n2 for l1 in lowercase for l2 in lowercase for n1 in digits for n2 in digits]
+print(answer[100])
+
