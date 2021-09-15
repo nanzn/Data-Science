a8b3c2291f81d34b84d7c407c6168790edc1bf5b
Zhou Nan <nanzn.zhou@gmail.com>
2021-09-15 14:35:04 +0800

    Completed Assignment 1

--- Coursera/Applied Data Science with Python Specialization/1 Intro to Data Science in Python/Week 1.py
+++ Coursera/Applied Data Science with Python Specialization/1 Intro to Data Science in Python/Week 1.py
@@ -1,7 +1,42 @@
-lowercase = 'abcdefghijklmnopqrstuvwxyz'
-digits = '0123456789'
+import re
+# Part A: Find a list of all of the names in the following string using regex.
+def names():
+    simple_string = """Amy is 5 years old, and her sister Mary is 2 years old. 
+    Ruth and Peter, their parents, have 3 kids."""
 
-# Generate a list of userid in format 'aa00' consisting of 2 lowercase and 2 digits
-answer = [l1+l2+n1+n2 for l1 in lowercase for l2 in lowercase for n1 in digits for n2 in digits]
-print(answer[100])
+    # All names start with a capital letter. 
+    # Find all words that begin with a capital letter
+    names = re.findall('[A-Z]{1}[a-z]+', simple_string)
+    return names
+
+# Part B: Create a regex to generate a list of just those students who received a B in the course.
+def grades():
+    with open ("grades.txt", "r") as file:
+        grades = file.read()
+
+    # All students got a B has ": B"
+    students = re.findall('([A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+)(?=\:\sB)', grades)
+    return students
+
+# Part C: Convert Log File into a list of dictionaries
+def logs():
+    with open("logdata.txt", "r") as file:
+        logdata = file.read()
+
+    loglines = logdata.split("\n") # Split by line
+    logs = [] # Create output list
+    for line in loglines[:-1]: # Exclude final blank line of file
+        # Use REGEX to pick out host, user_name, time and request
+        host = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line).group() # IP Address format
+        user_name = re.search('(?<=\s\-\s)([a-z0-9\-]+)', line).group() # Start with ' - '
+        time = re.search('(?<=\[)(.+)(?=\])', line).group() # Between []
+        request = re.search('(?<=\")(.+)(?=\")', line).group() # Between ""
+
+        # Create dictionary record
+        record = {"host": host, "user_name": user_name, "time": time, "request": request}
+
+        # Append record to list
+        logs.append(record)
+    
+    return logs
 
